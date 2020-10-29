aee6adfacdbbf2c3cdaccc3549cc44d6e462a21b
Andre <andre.cordeiro@youbyshoes.com.br>
2020-10-29 16:33:14 -0300

    Adição do codigo Analytics e troca do "insert" de lugar dos arquivos js

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -4,3 +4,14 @@
 // O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+
+
+// Código de escuta Analytics
+
+<script>
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-UA-12345-6', 'auto');
+ga('send', 'pageview');
+</script>
+<script async src='https://www.google-analytics.com/analytics.js'></script>
